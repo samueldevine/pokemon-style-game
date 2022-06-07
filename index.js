cd420e923136a4d1544d86f12e7bf0057feedfee
Sam Devine <sam.devine@pinnacol.com>
2022-06-07 13:23:41 -0600

    import and render map

--- index.js
+++ index.js
@@ -6,3 +6,10 @@ canvas.height = 576
 
 context.fillStyle = 'white'
 context.fillRect(0, 0, canvas.width, canvas.height)
+
+const image = new Image() // create html image object
+image.src = './img/Pellet Town.png'
+
+image.onload = () => {
+    context.drawImage(image, -750, -550)
+}
