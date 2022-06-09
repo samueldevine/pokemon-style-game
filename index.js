4ff444986570504f38fa2c737e41ce8541c257e6
Sam Devine <sam.devine@pinnacol.com>
2022-06-09 11:34:02 -0600

    create and render player

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 const canvas = document.querySelector('canvas')
-const context = canvas.getContext('2d')
+const c = canvas.getContext('2d')
 
 canvas.width = 1024
 canvas.height = 576
 
-context.fillStyle = 'white'
-context.fillRect(0, 0, canvas.width, canvas.height)
+c.fillStyle = 'white'
+c.fillRect(0, 0, canvas.width, canvas.height)
 
-const image = new Image() // create html image object
+const image = new Image()
 image.src = './img/Pellet Town.png'
 
+const playerImage = new Image()
+playerImage.src = './img/playerDown.png'
+
 image.onload = () => {
-    context.drawImage(image, -750, -550)
+    c.drawImage(image, -736, -600)
+    c.drawImage(
+        playerImage,
+        0,
+        0,
+        playerImage.width / 4,
+        playerImage.height,
+        canvas.width / 2 - playerImage.width / 4 / 2,
+        canvas.height / 2 - playerImage.height / 2,
+        playerImage.width / 4,
+        playerImage.height
+    )
 }
